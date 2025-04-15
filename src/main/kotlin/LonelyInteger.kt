9b93bb64a302852ea133bb261ccf9cbff4fed5aa
cusufcan <cusufcan@gmail.com>
2025-04-15 17:01:24 +0300

    fix: LonelyInteger

--- src/main/kotlin/LonelyInteger.kt
+++ src/main/kotlin/LonelyInteger.kt
@@ -1,4 +1,4 @@
-fun lonelyinteger(a: Array<Int>): Int {
+fun lonelyInteger(a: Array<Int>): Int {
     val countMap = mutableMapOf<Int, Int>()
     for (number in a) {
         countMap[number] = (countMap[number] ?: 0) + 1
